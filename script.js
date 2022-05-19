1960e08f0bbf7fa7f9df2cf6b7adbaa3cbc1c20c
BrumaAndrei <105850230+BrumaAndrei@users.noreply.github.com>
2022-05-19 22:38:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let gameMoves = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+let roundWon = false;
+let currentPlayer = "X";
+finalMessage.innerHTML = `It's ${currentPlayer}'s turn`;
+
+function PressFnc(id) {
+   if (document.getElementById(id).innerHTML === "" && gameActive === true) {
+      document.getElementById(id).innerHTML = currentPlayer;
+      gameMoves[id] = currentPlayer;
+      checkWhoWin();
+      PlayerChange();
+      finalMes();
+   }
+}
+
+function checkWhoWin() {
+   const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+   ];
+   for (let i = 0; i <= 7; i++) {
+      const PossibleMoves = winningConditions[i];
+      let box0 = gameMoves[PossibleMoves[0]];
+      let box1 = gameMoves[PossibleMoves[1]];
+      let box2 = gameMoves[PossibleMoves[2]];
+      if (box0 === box1 && box1 === box2 && box2 !== "") {
+         roundWon = true;
+         gameActive = false;
+         break
+      }
+   }
+}
+
+function PlayerChange() {
+   if (gameActive) {
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
+      finalMessage.innerHTML = `It's ${currentPlayer}'s turn`;
+   }
+}
+
+function finalMes() {
+   if (roundWon) {
+      finalMessage.innerHTML = `Player "${currentPlayer}" has won!`;
+   } else if (!gameMoves.includes("")) {
+      finalMessage.innerHTML = "Game ended in a draw!";
+   }
+}
